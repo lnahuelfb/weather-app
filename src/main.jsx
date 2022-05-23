8c54ef763ef398e7f55142ae5c20835b8d0cb8f6
Nahuel Fernandez Beschtedt <lnahuelfernandezb@gmail.com>
2022-05-23 18:29:32 -0300

    Inicio de la app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable linebreak-style */
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
